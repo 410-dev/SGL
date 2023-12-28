2dd83a1a3d1cc5a5a8df91ab23c5c7819800c388
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2023-12-28 22:32:42 +0900

    Added more events

--- src/main/java/me/hysong/sgl/interfaces/SGLEntityRunnable.java
+++ src/main/java/me/hysong/sgl/interfaces/SGLEntityRunnable.java
@@ -6,4 +6,8 @@ import me.hysong.sgl.panels.SGLPanel;
 @FunctionalInterface
 public interface SGLEntityRunnable {
     void run(SGLEntity entity, SGLPanel panel);
+    default void runPrior(SGLEntity entity, SGLPanel panel) {}
+    default void runPost(SGLEntity entity, SGLPanel panel) {}
+    default void runPriorAsync(SGLEntity entity, SGLPanel panel) {}
+    default void runPostAsync(SGLEntity entity, SGLPanel panel) {}
 }
